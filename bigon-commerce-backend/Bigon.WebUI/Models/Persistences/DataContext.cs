c48933d636bf8b251c5b87843a9ed3ab3430888d
isnuraina <nurane.ismayilzade19@gmail.com>
2025-06-13 21:47:49 +0400

    create-mvc-app

---
+++ bigon-commerce-backend/Bigon.WebUI/Models/Persistences/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bigon.WebUI.Models.Persistences
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
+    }
+}
---
+++ bigon-commerce-backend/Bigon.WebUI/Program.cs
@@ -0,0 +1,32 @@
+using Bigon.WebUI.Models.Persistences;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bigon.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DataContext>(
+                cfg =>
+                {
+                    cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"),
+                        opt =>
+                        {
+                            opt.MigrationsHistoryTable("Migrations");
+                        });
+                });
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+            });
+
+            app.Run();
+        }
+    }
+}
