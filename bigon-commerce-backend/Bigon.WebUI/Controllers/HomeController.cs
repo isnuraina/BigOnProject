7ce67c734cf2657b8d3b358bc2d8218320bf1b37
isnuraina <nurane.ismayilzade19@gmail.com>
2025-06-14 08:43:21 +0400

    Blog FrontToBack Shop FrontToBack

---
+++ bigon-commerce-backend/Bigon.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bigon.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
